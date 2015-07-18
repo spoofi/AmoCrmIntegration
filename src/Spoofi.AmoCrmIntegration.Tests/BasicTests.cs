eb6182539c993da64c6ec3f884948567e7f03221
Andrey Suvorov <spoofi.lab@gmail.com>
2015-07-18 15:28:08 +0500

    Вынес тесты для контактов в отдельный файл

--- src/Spoofi.AmoCrmIntegration.Tests/BasicTests.cs
+++ src/Spoofi.AmoCrmIntegration.Tests/BasicTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Spoofi.AmoCrmIntegration.Interface;
+﻿using Spoofi.AmoCrmIntegration.Interface;
 using Spoofi.AmoCrmIntegration.Service;
 using Spoofi.AmoCrmIntegration.Tests.Config;
 using Xunit;
@@ -9,9 +8,6 @@ namespace Spoofi.AmoCrmIntegration.Tests
     public class BasicTests
     {
         private readonly IAmoCrmService _service = new AmoCrmService(TestCrmConfig.Get());
-        private const string ContactName = "Геннадий";
-        private const int ContactId = 65682868;
-        private const int ResponsibleUserId = 144078;
 
         [Fact]
         public void GetAccountInfoTest()
@@ -20,37 +16,6 @@ namespace Spoofi.AmoCrmIntegration.Tests
             Assert.NotNull(accountInfo);
             Assert.NotNull(accountInfo.Id);
             Assert.NotNull(accountInfo.Name);
-        }
-
-        [Fact]
-        public void GetAllContactsTest()
-        {
-            var contacts = _service.GetContacts();
-            Assert.True(contacts.Any());
-        }
-
-        [Fact]
-        public void GetContactByIdTest()
-        {
-            var contact = _service.GetContact(ContactId);
-            Assert.NotNull(contact);
-            Assert.Equal(ContactId, contact.Id);
-        }
-
-        [Fact]
-        public void GetContactsByQueryTest()
-        {
-            var contacts = _service.GetContacts(ContactName);
-            Assert.NotNull(contacts);
-            Assert.True(contacts.Any(c => c.Name.Contains(ContactName)));
-        }
-
-        [Fact]
-        public void GetContactsByResponsibleUserIdTest()
-        {
-            var contacts = _service.GetContacts(ResponsibleUserId);
-            Assert.NotNull(contacts);
-            Assert.True(contacts.All(c => c.ResponsibleUserId == ResponsibleUserId));
         }
     }
 }
---
+++ src/Spoofi.AmoCrmIntegration.Tests/ContactsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Spoofi.AmoCrmIntegration.Interface;
+using Spoofi.AmoCrmIntegration.Service;
+using Spoofi.AmoCrmIntegration.Tests.Config;
+using Xunit;
+
+namespace Spoofi.AmoCrmIntegration.Tests
+{
+    public class ContactsTests
+    {
+        private readonly IAmoCrmService _service = new AmoCrmService(TestCrmConfig.Get());
+        private const string ContactName = "Геннадий";
+        private const int ContactId = 65682868;
+        private const int ResponsibleUserId = 144078;
+
+        [Fact]
+        public void GetAllContactsTest()
+        {
+            var contacts = _service.GetContacts();
+            Assert.True(contacts.Any());
+        }
+
+        [Fact]
+        public void GetContactByIdTest()
+        {
+            var contact = _service.GetContact(ContactId);
+            Assert.NotNull(contact);
+            Assert.Equal(ContactId, contact.Id);
+        }
+
+        [Fact]
+        public void GetContactsByQueryTest()
+        {
+            var contacts = _service.GetContacts(ContactName);
+            Assert.NotNull(contacts);
+            Assert.True(contacts.Any(c => c.Name.Contains(ContactName)));
+        }
+
+        [Fact]
+        public void GetContactsByResponsibleUserIdTest()
+        {
+            var contacts = _service.GetContacts(ResponsibleUserId);
+            Assert.NotNull(contacts);
+            Assert.True(contacts.All(c => c.ResponsibleUserId == ResponsibleUserId));
+        }
+    }
+}
